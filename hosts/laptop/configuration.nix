e93916a7bbf0a3dcd16466bc853f5d8123ed3a69
astroreen <astroreen@gmail.com>
2025-07-30 06:53:35 +0300

    restructured hosts/laptop/configuration.nix

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,69 +1,47 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+  imports = [
+      ./hardware-configuration.nix  # Required.
       
-      ../../modules/wm/hyprland
+      ../../modules/wm/hyprland     # Window managers
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
+  networking.hostName = "laptop"; # Define your hostname.
   networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Vilnius";
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.astroreen = {
+    isNormalUser = true;
+    description = "astroreen";
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+  };
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
-  # Enable the KDE Plasma Desktop Environment.
-  # services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  i18n.extraLocales = [ "ru_RU.UTF-8/UTF-8" "lt_LT.UTF-8/UTF-8" ];
 
   # Configure keymap in X11
   services.xserver.xkb = {
-    layout = "us";
-    variant = "";
+    layout = "us,ru,lt";
+    variant = ",phonetic,us";
+    options = "terminate:ctrl_alt_bksp";
+    model = "pc104";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  services.displayManager = {
-    gdm = {
-      enable = true;
-      wayland = true;
-    };
-  };
-  
-  programs = {
-    hyprland = {
-      enable = true;
-      xwayland.enable = true;
-    };
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  # Audio
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -71,106 +49,89 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    jack.enable = false;
   };
 
-  # polkit
-  security.polkit.enable = true;
-  # keyring
-  services.gnome.gnome-keyring.enable = true;
-  security.pam.services.gdm-password.enableGnomeKeyring = true;
-  # enables trash
-  services.gvfs.enable = true;
-  # waybar icons
-  fonts.packages = with pkgs; [                                                                          
-    font-awesome
-  ];
-  # power-profiles-daemon
-  services.power-profiles-daemon.enable = true;
+  # Display manager
+  services.displayManager = {
+    # CAUTION: DO NOT ENABLE BOTH!
+    sddm.enable = false;
+    gdm = {
+      enable = true;
+      wayland = true;
+    };
+
+    # Enable automatic login for the user.
+    autoLogin = {
+      enable = true;
+      user = "astroreen";
+    };
+  };
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.astroreen = {
-    isNormalUser = true;
-    description = "astroreen";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
+  # Desktop Environments
+  services.desktopManager.plasma6.enable = true;
+  programs = {
+    hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
   };
 
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "astroreen";
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # System wide packages
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    tree
-    git
-    localsend
-    # file manager
-    nautilus
-    # console
-    kitty
-    # app launcher
-    walker
-    # wallapaper
-    hyprpaper
-    # keyring
-    seahorse
-    # theming
-    nwg-look
-    # screenshot
-    hyprshot
-    # clipboard
-    wl-clip-persist
+    nautilus              # File manager
+    kitty                 # Console
+    walker                # App launcher
+    hyprpaper             # Wallapaper
+    seahorse              # Keyring
+    nwg-look              # Theming
+    hyprshot              # Screenshot
+    wl-clip-persist       # Clipboard
 
-    # utils
-    brightnessctl
-    playerctl
-    networkmanagerapplet
+    # System utilities
+    brightnessctl         # Brightness controls
+    playerctl             # Music player control
+    networkmanagerapplet  # Network Manager applet
     pavucontrol
+    tree                  # `tree` command
 
-    # others
-    gnome-calculator
-    gnome-text-editor
+    # Others
+    gnome-calculator      # Calculator
+    gnome-text-editor     # Gnome text editor
+    vim                   # Vim text editor
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  
+  # Fonts
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono                                                                     
+    font-awesome
+  ];
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  # Security
+  security.polkit.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.gdm-password.enableGnomeKeyring = true;
+  services.gvfs.enable = true; # Enables trash
+  services.power-profiles-daemon.enable = true;
+
+  # Install firefox. Default.
+  programs.firefox.enable = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
