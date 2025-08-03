88b6a241ec967701c52370965b548181d42ef507
astroreen <astroreen@gmail.com>
2025-08-03 11:31:56 +0300

    Fixed the NeoHtop issue with displaying context

--- modules/tui/htop/default.nix
+++ modules/tui/htop/default.nix
@@ -5,7 +5,18 @@
         package = pkgs.neohtop;
     };
 
+    # Create a wrapper script with environment variables
+    home.packages = [
+        (pkgs.writeShellScriptBin "neohtop-fixed" ''
+            export GDK_BACKEND=x11
+            export WEBKIT_DISABLE_DMABUF_RENDERER=1
+            exec ${pkgs.neohtop}/bin/NeoHtop "$@"
+        '')
+    ];
+
+    # Set up proper aliases
     home.shellAliases = {
-        htop = "NeoHtop";
+        htop = "neohtop-fixed";
+        NeoHtop = "neohtop-fixed";
     };
 }
