eb1b1d0b9c190db23902cead7fc4ab80926889c5
astroreen <astroreen@gmail.com>
2025-09-24 16:50:52 +0300

    Enabled neohtop back on

--- modules/tui/htop/default.nix
+++ modules/tui/htop/default.nix
@@ -2,21 +2,21 @@
 {
     programs.htop = {
         enable = true;
-        # package = pkgs.neohtop;
+        package = pkgs.neohtop;
     };
 
     # Create a wrapper script with environment variables
-    # home.packages = [
-    #    (pkgs.writeShellScriptBin "neohtop-fixed" ''
-    #         export GDK_BACKEND=x11
-    #         export WEBKIT_DISABLE_DMABUF_RENDERER=1
-    #         exec ${pkgs.neohtop}/bin/NeoHtop "$@"
-    #     '')
-    # ];
+    home.packages = [
+       (pkgs.writeShellScriptBin "neohtop-fixed" ''
+            export GDK_BACKEND=x11
+            export WEBKIT_DISABLE_DMABUF_RENDERER=1
+            exec ${pkgs.neohtop}/bin/NeoHtop "$@"
+        '')
+    ];
 
     # Set up proper aliases
-    # home.shellAliases = {
-    #     htop = "neohtop-fixed";
-    #     NeoHtop = "neohtop-fixed";
-    # };
+    home.shellAliases = {
+        htop = "neohtop-fixed";
+        NeoHtop = "neohtop-fixed";
+    };
 }
