ff8bd0d0aa736a046b7b79023ed310ca01b93fd0
astroreen <astroreen@gmail.com>
2026-03-03 06:38:42 +0200

    Fixed neohtop

--- home/modules/tui/htop.nix
+++ home/modules/tui/htop.nix
@@ -1,11 +1,5 @@
 { pkgs, ... }:
 {
-  programs.htop = {
-    enable = true;
-    package = pkgs.neohtop;
-  };
-
-  # Create a wrapper script with environment variables
   home.packages = [
     (pkgs.writeShellScriptBin "neohtop-fixed" ''
       export GDK_BACKEND=x11
@@ -14,7 +8,22 @@
     '')
   ];
 
-  # Set up proper aliases
+  # This is the missing piece for the "Application" menu
+  xdg.desktopEntries = {
+    neohtop = {
+      name = "NeoHtop";
+      genericName = "System Monitor";
+      exec = "neohtop-fixed";
+      terminal = false;
+      categories = [
+        "System"
+        "Monitor"
+      ];
+      icon = "htop"; # Or the specific icon name if you have one
+      comment = "NeoHtop with Hyprland fixes";
+    };
+  };
+
   home.shellAliases = {
     htop = "neohtop-fixed";
     NeoHtop = "neohtop-fixed";
