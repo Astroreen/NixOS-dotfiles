c0e2bffc77806e0cf3b5784368a8afc955fdb7d1
astroreen <astroreen@gmail.com>
2026-03-11 03:08:08 +0200

    Fixing caelestia configuration not applying changes properly

--- home/astroreen/common/hyprland/caelestia/default.nix
+++ home/astroreen/common/hyprland/caelestia/default.nix
@@ -21,28 +21,32 @@ in
 
   config = lib.mkIf cfg.enable {
 
-    programs.caelestia.systemd.environment = [
-      "QT_QPA_PLATFORM=wayland;xcb"
-      "XDG_SESSION_TYPE=wayland;xcb"
-      "XDG_CURRENT_DESKTOP=Hyprland"
-      "XDG_SESSION_DESKTOP=Hyprland"
-      "QT_QPA_PLATFORMTHEME=qt6ct"
-      "XDG_RUNTIME_DIR=/run/user/1000"
-      "WAYLAND_DISPLAY=wayland-1"
-      "QUICKSHELL_ENABLE_PORTAL=1"
-    ];
+    programs.caelestia = {
+      settings = { }; # keep empty so official module skips writing shell.json
+      extraConfig = ""; # keep empty
+      systemd.environment = [
+        "QT_QPA_PLATFORM=wayland;xcb"
+        "XDG_SESSION_TYPE=wayland;xcb"
+        "XDG_CURRENT_DESKTOP=Hyprland"
+        "XDG_SESSION_DESKTOP=Hyprland"
+        "QT_QPA_PLATFORMTHEME=qt6ct"
+        "XDG_RUNTIME_DIR=/run/user/1000"
+        "WAYLAND_DISPLAY=wayland-1"
+        "QUICKSHELL_ENABLE_PORTAL=1"
+      ];
+    };
 
     # Home activation is used here to not symlink file into nix store, thus making it writable
     home.activation.caelestiaConfig =
       let
-        mergedConfig = cfg.settings // {
-          # Additional configuration
+        # Additional configuration
+        overrides = {
           paths = {
             mediaGif = ../../assets/kirby-groove.gif;
             sessionGif = ../../assets/evernight-dance.gif;
           };
         };
-
+        mergedConfig = lib.recursiveUpdate cfg.settings overrides;
         configFile = pkgs.writeText "caelestia-config" (builtins.toJSON mergedConfig);
       in
       lib.hm.dag.entryAfter [ "writeBoundary" ] ''
@@ -52,10 +56,7 @@ in
         if [ ! -f "$CONF" ] || ! diff -q "$CONF" "$NEW" > /dev/null 2>&1; then
           cp "$NEW" "$CONF"
           chmod 644 "$CONF"
-          echo "Caelestia config changed, restarting service..."
           systemctl --user restart caelestia || true
-        else
-          echo "Caelestia config unchanged, skipping restart."
         fi
       '';
   };
