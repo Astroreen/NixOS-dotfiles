6df6589a262e1820e1582a7e684d86a4be1ace25
astroreen <astroreen@gmail.com>
2026-05-17 19:47:29 +0300

    Updated vnc provider and also configured some of the keybinds

--- home/modules/gui/vnc.nix
+++ home/modules/gui/vnc.nix
@@ -1,7 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  format = pkgs.formats.keyValue { };
+in
 {
   services.wayvnc = {
-    enable = true;
+    enable = false;
     autoStart = true;
     settings = {
       address = "0.0.0.0";
@@ -10,12 +13,52 @@
   };
 
   home.packages = with pkgs; [
-    realvnc-vnc-viewer
+    wayvnc  # VNC server for Wayland
+    tigervnc # VNC viewer 
   ];
 
-  wayland.windowManager.hyprland.settings.windowrule = [
-    # VNC viewer should float and be centered
-    "match:class ^(realvnc-vncviewer)$, float on, center on"
-    "match:class ^(Vncviewer)$, float on, center on" # VNC viewer should float and be centered
-  ];
+  systemd.user.services."wayvnc" = {
+    Unit = {
+      Description = "wayvnc VNC server";
+      Documentation = [ "man:wayvnc(1)" ];
+      After = [
+        config.wayland.systemd.target
+      ];
+      PartOf = [
+        config.wayland.systemd.target
+      ];
+    };
+    Service = {
+      ExecStart = [ "${pkgs.wayvnc}/bin/wayvnc 0.0.0.0 5900" ];
+      Restart = "on-failure";
+      RestartSec = "3s";
+    };
+    Install.WantedBy = [
+      config.wayland.systemd.target
+    ];
+  };
+
+  xdg.configFile."wayvnc/config".source = format.generate "wayvnc.conf" {
+    address = "0.0.0.0";
+    port = 5900;
+  };
+
+  wayland.windowManager.hyprland = {
+    submaps = {
+      passthrough.settings.bind = [
+        "SUPER, F12, submap, global"
+      ];
+
+      global.settings.bind = [
+        # Cycle wayvnc output across monitors (wayvncctl is bundled with wayvnc)
+        "CTRL SUPER SHIFT, SEMICOLON, exec, wayvncctl output-cycle"
+        "SUPER, F12, submap, passthrough"
+      ];
+    };
+    settings.windowrule = [
+      # VNC viewer should float and be centered
+      "match:class ^(realvnc-vncviewer)$, float on, center on"
+      "match:class ^(Vncviewer)$, float on, center on" # VNC viewer should float and be centered
+    ];
+  };
 }
