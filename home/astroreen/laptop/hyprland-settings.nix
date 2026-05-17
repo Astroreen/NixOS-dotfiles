4f03e1551f1748dfeb9de8a840eee07b2efbf5b3
astroreen <astroreen@gmail.com>
2026-05-17 11:36:37 +0300

    Adding gestures to the laptop

--- home/astroreen/laptop/hyprland-settings.nix
+++ home/astroreen/laptop/hyprland-settings.nix
@@ -2,4 +2,14 @@
   monitor = [
     "eDP-1,1920x1080@165,0x0,1"
   ];
+
+  gestures = {
+    workspace_swipe = true;
+    workspace_swipe_fingers = 4;
+    workspace_swipe_distance = 300;
+    workspace_swipe_invert = true;
+    workspace_swipe_cancel_ratio = 0.5;
+    workspace_swipe_create_new = true; # Create new workspace when swiping beyond the last workspace
+    workspace_swipe_forever = false;
+  };
 }
