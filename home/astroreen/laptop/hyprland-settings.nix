b383c4e56a3ff9e30f6053134eaee508217e9c60
astroreen <astroreen@gmail.com>
2026-05-17 12:07:19 +0300

    Changing gesture configuration since hyprland configuration has changed

--- home/astroreen/laptop/hyprland-settings.nix
+++ home/astroreen/laptop/hyprland-settings.nix
@@ -4,12 +4,14 @@
   ];
 
   gestures = {
-    workspace_swipe = true;
-    workspace_swipe_fingers = 4;
     workspace_swipe_distance = 300;
     workspace_swipe_invert = true;
     workspace_swipe_cancel_ratio = 0.5;
     workspace_swipe_create_new = true; # Create new workspace when swiping beyond the last workspace
     workspace_swipe_forever = false;
   };
+
+  gesture = [
+    "4, horizontal, workspace"
+  ];
 }
